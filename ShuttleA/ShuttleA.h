fcb79f8d2c6ab830e83f68dbaab6a0b6b824c039
Zappyford <antoinechenon@yahoo.fr>
2014-05-29 18:20:44 +0200

    Bug fixed. New automata : Take Off
    The Shuttle A can now take off  by itself !

--- ShuttleA/ShuttleA.h
+++ ShuttleA/ShuttleA.h
@@ -142,13 +142,13 @@ public:
 private:
 	/* OTTO */
 
-	void maintainAltitude(double goal);
-	void test();
-	void STATE_TOTO(double simt, double simdt, double mjd);
 	void STATE_1(double simt, double simdt, double mjd);
+	void STATE_2(double simt, double simdt, double mjd);
+	void STATE_3(double simt, double simdt, double mjd);
 	int m_state = 0;
 	void postStep(double simt, double simdt, double mjd);
 	VECTOR3 tmp_vector;
+
 
 	/* END_OTTO */
 
